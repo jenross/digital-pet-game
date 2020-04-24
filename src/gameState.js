bf6df83861c2442780d1b32f4c1182d4b3ae8db7
jenross <ross.m.jennifer@gmail.com>
2020-04-24 17:01:46 -0400

    game started

--- src/gameState.js
+++ src/gameState.js
@@ -1,14 +1,66 @@
+import { modFox, modScene } from "./ui";
+import { RAIN_CHANCE, SCENES } from "./constants";
+
 const gameState = {
   current: "INIT",
   clock: 1,
+  wakeTime: -1,
   tick() {
     this.clock++;
     console.log("clock", this.clock);
+
+    if (this.clock === this.wakeTime) {
+      this.wake();
+    }
     return this.clock;
   },
+  startGame() {
+    this.current = "HATCHING";
+    this.wakeTime = this.clock + 3;
+    modFox("egg");
+    modScene("day");
+  },
+  wake() {
+    this.current = "IDLING";
+    this.wakeTime = -1;
+    modFox("idling");
+    this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
+    modScene(SCENES[this.scene]);
+  },
   handleUserAction(icon) {
-    console.log(icon);
+    if (
+      ["SLEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)
+    ) {
+      //do nothing
+      return;
+    }
+
+    if (this.current === "INIT" || this.current === "DEAD") {
+      this.startGame();
+      return;
+    }
+
+    switch (icon) {
+      case "weather":
+        this.changeWeather();
+        break;
+      case "poop":
+        this.cleanUpPoop();
+        break;
+      case "fish":
+        this.feed();
+        break;
+    }
+  },
+  changeWeather() {
+    console.log("change weather");
+  },
+  cleanUpPoop() {
+    console.log("clean up poop");
+  },
+  feed() {
+    console.log("feed");
   },
 };
-
+export const handleUserAction = gameState.handleUserAction.bind(gameState);
 export default gameState;
---
+++ src/ui.js
@@ -0,0 +1,13 @@
+export const modFox = function modFox(state) {
+  document.querySelector('.fox').className = `fox fox-${state}`;
+}
+//put function name again for debugging (will show up in call stack)
+export const modScene = function modScene(state) {
+  document.querySelector('.game').className = `game ${state}`;
+}
+
+export const togglePoopBag = function togglePoopBag(show) {
+  document.querySelector('.poop-bag').classList.toggle('hidden', !show);
+};
+
+
